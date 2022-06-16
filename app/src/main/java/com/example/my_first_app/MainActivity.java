29cf8a56eff3ba8b80fae4ad0dd80b2ccd6c90ca
V.yundt <vladimiryundtz@gmail.com>
2022-06-16 13:20:08 +0700

    add class (word answer question)

--- app/src/main/java/com/example/my_first_app/MainActivity.java
+++ app/src/main/java/com/example/my_first_app/MainActivity.java
@@ -1,32 +1,54 @@
 package com.example.my_first_app;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import java.sql.*;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
-
-    private TextQuestion textQuestion;
-    private But buttonGoToProperty;
-
+    public int a = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        But = findViewById(R.id.buttonGoToProperty);
-        TextQuestion = findViewById(R.id.textQuestion);
+    public void onButtonClick1 (View v) {
+        TextView b = findViewById(R.id.textScore);
+        a = a + 1;
+        b.setText("aaa"+Integer.toString(a));
+         // System.out.print(v)
+    }
 
-        But.setOnClockListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextQuestion.TextView = 'gerere'
+    public void onButtonClick2 (View v) {
+        TextView b = findViewById(R.id.textScore);
+        a = a + 10;
+        a = word.testa(a);
+        b.setText("aaa"+Integer.toString(a));
 
-            }
-        });
+        // System.out.print(v);
+
+    }
+
+    public void onButtonClick3 (View v) {
+        TextView b = findViewById(R.id.textScore);
+        a = a + 100;
+        b.setText("aaa"+Integer.toString(a));
+        // System.out.print(v);
+
+    }
+
+    public void onButtonClick4 (View v) {
+        TextView b = findViewById(R.id.textScore);
+        a = a + 100;
+        b.setText("aaa"+Integer.toString(a));
+        // System.out.print(v);
+
     }
 }
-
-public class Aaaa
---
+++ app/src/main/java/com/example/my_first_app/Word.java
@@ -0,0 +1,12 @@
+package com.example.my_first_app;
+
+import java.sql.*;
+
+class word {
+    public static int testa (int b){
+
+        b = b +1000;
+        return b;
+
+    }
+}
